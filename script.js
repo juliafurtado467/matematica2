02fa43fcb77fe035ed39301ef83d65af5fccace9
Helena Frizoni <helenafrizoni20@gmail.com>
2026-03-25 09:09:03 -0300

    foi realizado a multiplicacao

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// inicio
+let numUm
+let numDois
+let resultado
+
+numUm = Number(prompt("digite o primeiro numero"));
+numDois = Number(prompt("digite o segundo numero"));
+
+resultado = numUm * numDois;
+
+alert(`o resultado será ${resultado}`);
