81d7d39f58de2f5ca964bc879291c477b1455c3c
Julia Furtado <juliafurtado467@gmail.com>
2026-03-25 08:39:26 -0300

    Realizado a operação de subtração

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// inicio
+let numUm
+let numDois
+let resultado
+
+numUm = Number(prompt("Qual o primeiro número?"))
+numDois = Number(prompt("Qual o segundo número?"))
+
+resultado = numUm -  numDois
+
+alert(`O resultado da subtração é: ${resultado}`)
