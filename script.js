8841714d7e221c29584e9bbd9bec97b6a39e9061
Helena Frizoni <helenafrizoni20@gmail.com>
2026-03-25 09:38:13 -0300

    mudanca de variavel

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let numUm
 let numDois
-let resultado
+let resultadoDaMultiplicacao
 
 numUm = Number(prompt("digite o primeiro numero"));
 numDois = Number(prompt("digite o segundo numero"));
 
-resultado = numUm * numDois;
+resultadoDaMultiplicacao = numUm * numDois;
 
-alert(`o resultado será ${resultado}`);
+alert(`o resultado será ${resultadoDaMultiplicacao}`);
