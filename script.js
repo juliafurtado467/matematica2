657eac909b31c8cd3ba55dc0ac299934df753d97
Nicolas bani <nicolassbani11@gmail.com>
2026-03-25 08:54:55 -0300

    commit da operação adição

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// inicio
+let numUm
+let numDois
+let resultado 
+
+numUm = Number(prompt("qual seria o primeiro número"));
+numDois = Number(prompt("qual seria o segundo número"));
+
+resultado = numUm + numDois 
+
+alert(` o resultado da adição será: ${resultado}`)
