62203c9727640d538d445f3aa0cedd70dea92317
Ana Clara <schultzana192@gmail.com>
2026-03-25 08:54:35 -0300

    commit da operação de divisão

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// inicio
+let numUm
+let numDois
+let result
+
+numUm = Number(prompt("escreva o primeiro numero"));
+numDois = Number(prompt("escreva o segundo numero"));
+
+result = numUm/numDois;
+
+alert(`o resultado da divisão será ${result}`);
